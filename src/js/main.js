88d3c8751624732ee840eb33ca359b26aea34598
Alexander Gerdt <info@gerdt-webdesign.de>
2023-12-05 09:52:50 +0100

    Teil 1 Projekt Variablen anlegen

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let titel_1 = prompt("Titel:");
+let typ_1 = prompt("Typ (Einnahme oder Ausgabe)");
+let betrag_1 = prompt("Betrag: (in Cent)");
+let datum_1 = prompt("Datum (jjjj-mm-tt)");
+
+console.log(titel_1, typ_1, betrag_1, datum_1);
+
+let einnahmen;
+let ausgaben;
+let bilanz;
---
+++ ueben.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let stadt = "Bonn";
+
+console.log(stadt);
+stadt = 39;
+
+console.log(stadt);
