609bb37e3957b55ecab0889085ab272ed696421c
Alexander Gerdt <info@gerdt-webdesign.de>
2024-01-18 02:55:17 +0100

    TEil 13 Projekt - HTML Ausgaben generieren

--- src/js/main.js
+++ src/js/main.js
@@ -31,7 +31,7 @@ const haushaltsbuch = {
     },
 
     typ_validieren(typ){
-        if( typ.match(/^(?:e|a)$/) !== null){
+        if( typ.match(/^(?:einnahme|ausgabe)$/) !== null){
             return true;
         } else {
             return false;
@@ -121,6 +121,57 @@ const haushaltsbuch = {
         });
     },
 
+    html_eintrag_generieren(eintrag){
+
+        let listenpunkt = document.createElement("li");
+
+        listenpunkt.setAttribute("class", eintrag.get("typ"));
+        listenpunkt.setAttribute("data-timestamp", eintrag.get("id"));
+
+        let datum = document.createElement("span");
+        datum.setAttribute("class", "datum");
+        datum.textContent = eintrag.get("datum").toLocaleString("de-DE", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            });
+        listenpunkt.insertAdjacentElement("afterbegin", datum);
+        
+        let titel = document.createElement("span");
+        titel.setAttribute("class", "titel");
+        titel.textContent = eintrag.get("titel");
+        datum.insertAdjacentElement("afterend", titel);
+
+        let betrag = document.createElement("span");
+        betrag.setAttribute("class", "betrag");
+        betrag.textContent = (eintrag.get("betrag")/100).toFixed(2).replace(/\./, ",") + "€";
+        titel.insertAdjacentElement("afterend", betrag);
+
+        let button = document.createElement("button");
+        button.setAttribute("class", "entfernen-button");
+        betrag.insertAdjacentElement("afterend", button);
+
+        let i = document.createElement("i");
+        i.setAttribute("class", "fas");
+        i.classList.add("fa-trash");
+        button.insertAdjacentElement("afterbegin", i);
+        
+        return listenpunkt;
+        
+    },
+
+    eintraege_anzeigen(){
+        document.querySelectorAll(".monatsliste ul").forEach(e => {
+            e.remove();
+        });
+        let eintragsliste = document.createElement("ul");
+
+        for (let eintrag of this.eintraege){
+            eintragsliste.insertAdjacentElement("beforeend",  this.html_eintrag_generieren(eintrag));
+        };
+        document.querySelector(".monatsliste").insertAdjacentElement("afterbegin", eintragsliste);
+    },
+
 
     gesamtbilanz_erstellen(){
         let neue_gesamtbilanz = new Map();
@@ -130,11 +181,11 @@ const haushaltsbuch = {
 
         this.eintraege.forEach(function(eintrag){
             switch (eintrag.get("typ")){
-                case "e":
+                case "einnahme":
                     neue_gesamtbilanz.set("einnahmen", neue_gesamtbilanz.get("einnahmen") + eintrag.get("betrag"));
                     neue_gesamtbilanz.set("bilanz", neue_gesamtbilanz.get("bilanz") + eintrag.get("betrag"));
                 break;
-                case "a":
+                case "ausgabe":
                     neue_gesamtbilanz.set("ausgaben", neue_gesamtbilanz.get("ausgaben") + eintrag.get("betrag"));
                     neue_gesamtbilanz.set("bilanz", neue_gesamtbilanz.get("bilanz") - eintrag.get("betrag"));
                     break;
@@ -146,11 +197,60 @@ const haushaltsbuch = {
         this.gesamtbilanz = neue_gesamtbilanz;
     },
 
-    gesamtbilanz_ausgeben(){
-        console.log(`Einnahmen: ${(this.gesamtbilanz.get("einnahmen")/100).toFixed(2)} €\n`
-            + `Ausgaben: ${(this.gesamtbilanz.get("ausgaben")/100).toFixed(2)} €\n`
-            + `Bilanz: ${(this.gesamtbilanz.get("bilanz")/100).toFixed(2)} €\n`
-            + `Bilanz ist positiv: ${this.gesamtbilanz.get("bilanz") >= 0}`);
+    html_gesamtbilanz_generieren(){
+
+        let gesamtbilanz = document.createElement("aside");
+        gesamtbilanz.setAttribute("id", "gesamtbilanz");
+
+        let ueberschrift = document.createElement("h1");
+        ueberschrift.textContent = "Gesamtbilanz";
+        gesamtbilanz.insertAdjacentElement("afterbegin", ueberschrift);
+
+        let einnahmenZeile = document.createElement("div");
+        einnahmenZeile.setAttribute("class", "gesamtbilanz-zeile einnahmen");
+        let einnahmenTitel = document.createElement("span");
+        einnahmenTitel.textContent = "Einnahmen:";
+        einnahmenZeile.insertAdjacentElement("afterbegin", einnahmenTitel);
+        let einnahmenBetrag = document.createElement("span");
+        einnahmenBetrag.textContent = (this.gesamtbilanz.get("einnahmen")/100).toFixed(2).replace(/\./, "," + "€");
+        einnahmenZeile.insertAdjacentElement("beforeend", einnahmenBetrag);
+        gesamtbilanz.insertAdjacentElement("beforeend", einnahmenZeile);
+
+        let ausgabenZeile = document.createElement("div");
+        ausgabenZeile.setAttribute("class", "gesamtbilanz-zeile ausgaben");
+        let ausgabenTitel = document.createElement("span");
+        ausgabenTitel.textContent = "Ausgaben:";
+        ausgabenZeile.insertAdjacentElement("afterbegin", ausgabenTitel);
+        let ausgabenBetrag = document.createElement("span");
+        ausgabenBetrag.textContent = (this.gesamtbilanz.get("ausgaben")/100).toFixed(2).replace(/\./, "," + "€");
+        ausgabenZeile.insertAdjacentElement("beforeend", ausgabenBetrag);
+        gesamtbilanz.insertAdjacentElement("beforeend", ausgabenZeile);
+
+        let bilanzZeile = document.createElement("div");
+        bilanzZeile.setAttribute("class", "gesamtbilanz-zeile bilanz");
+        let bilanzTitel = document.createElement("span");
+        bilanzTitel.textContent = "Bilanz:";
+        bilanzZeile.insertAdjacentElement("afterbegin", bilanzTitel);
+        let bilanzBetrag = document.createElement("span");
+        if (this.gesamtbilanz.get("bilanz") >= 0){
+            bilanzBetrag.setAttribute("class", "positiv");
+        } else if (this.gesamtbilanz.get("bilanz") < 0){
+            bilanzBetrag.setAttribute("class", "negativ");
+        };
+        bilanzBetrag.textContent = (this.gesamtbilanz.get("bilanz")/100).toFixed(2).replace(/\./, "," + "€");
+        bilanzZeile.insertAdjacentElement("beforeend", bilanzBetrag);
+        gesamtbilanz.insertAdjacentElement("beforeend", bilanzZeile);
+
+        return gesamtbilanz;
+
+    },
+
+    gesamtbilanz_anzeigen(){
+        let gesamtbilanz = document.querySelector("#gesamtbilanz");
+        gesamtbilanz.remove();
+
+        document.querySelector("body").insertAdjacentElement("beforeend", this.html_gesamtbilanz_generieren());
+        
     },
 
     eintrag_hinzufuegen(){
@@ -159,9 +259,9 @@ const haushaltsbuch = {
             this.eintrag_erfassen();
             if(this.fehler.length == 0){
                 this.eintraege_sortieren();
-                this.eintraege_ausgeben();
+                this.eintraege_anzeigen();
                 this.gesamtbilanz_erstellen();
-                this.gesamtbilanz_ausgeben();
+                this.gesamtbilanz_anzeigen();
             } else {
                 this.fehler = [];
             }
--- ueben.js
+++ ueben.js
@@ -1,35 +1,151 @@
 "use strict"
 
-// let s = " Lorem ipsum, dolor sit amet ipsum. ";
+// let div = document.createElement("div");
+// div.setAttribute("id", "meine_id");
+// div.setAttribute("class", "meine_klasse");
+// let text = document.createTextNode("Lorem ipsum dolor sit amet. Du Affe.");
 
-// console.log(s.length);
-// console.log(s.indexOf("ipsum", 20));
-// console.log(s.lastIndexOf("ipsum"));
-// console.log(s.includes("amet", 25));
-// console.log(s.toLocaleLowerCase());
-// console.log(s.toLocaleUpperCase());
-// console.log(s.trim());
-// console.log(s.repeat(5));
+// div.appendChild(text);
 
-// let reg = /i\w{4}/g;
+// console.log(div);
 
-// console.log(s.search(reg));
-// console.log(s.replace(reg, "Opsunar--------------"));
-// console.log(s.replace(/,/, "....."));
-// console.log(s.replace(",", "..."));
+// let jumbo = document.querySelector(".jumbotron > section");
 
-// console.log(s.match(reg));
+// jumbo.appendChild(div);
 
-// let zahl = Math;
-// zahl = zahl.random()*10;
+// let listenpunkt = document.createElement("li");
+// listenpunkt.setAttribute("id", "meinListenElement");
+// let ankerElement = document.createElement("a");
+// ankerElement.setAttribute("id", "meinAnkerElement");
+// ankerElement.setAttribute("href", "#");
+// let text_2 = document.createTextNode("Element");
+// ankerElement.appendChild(text_2);
+// ankerElement.innerHTML = "Element";
+// listenpunkt.appendChild(ankerElement);
 
-// console.log(Math.ceil(zahl));
+// console.log(listenpunkt);
 
-// console.log( isNaN(typ));
+// let liste = document.querySelector("#navigation > ul li:nth-of-type(2)");
+
+// liste.appendChild(listenpunkt);
+
+// liste.insertAdjacentElement("afterbegin", listenpunkt);
+
+// let domString = "<li><a id=\"meinAnkerElement\" href=\"#\">Element</a></li>";
+
+// liste.insertAdjacentHTML("afterbegin", domString);
+
+// liste.insertAdjacentElement("beforebegin", listenpunkt);
+
+// let nav = document.querySelector("#navigation");
+
+// nav.remove();
+
+// let jumbo = document.querySelectorAll(".jumbotron");
+// console.log(jumbo)
+
+// jumbo.forEach(e => {
+//     e.remove();
+// })
+
+// let navigation = document.querySelector("#navigation");
+// let id = navigation.id;
+// console.log(id);
+// navigation.id = "neue_id";
+// navigation.id = "";
+
+// let jumbo = document.querySelector(".jumbotron");
+
+// let classes = jumbo.className;
+
+// console.log(classes);
+
+// jumbo.className = "neueKlasse";
+
+// let aktivKlasse = document.querySelector(".active");
+
+// let klasseWeg = document.querySelector("#navigation > ul > li > a").className = "";
+
+// let klasseDa = document.querySelector("#navigation > ul li:nth-of-type(3) > a").className = "active";
 
 
-let fehler = [];
+// console.log(aktivKlasse);
 
-fehler.push("sdes");
+// let jumbo = document.querySelector("body .jumbotron");
+// let jumbo = document.querySelector(".jumbotron:nth-of-type(2)");
 
-console.log(fehler.length);
+// let class_List = jumbo.classList;
+// jumbo.classList.add("meine-klasse");
+// jumbo.classList.remove("jumbotron");
+// jumbo.classList.replace("meine-klasse", "deine-klasse");
+// console.log(jumbo.classList.contains("deine-klasse"));
+// jumbo.classList.toggle("SchalterAn");
+// jumbo.classList.toggle("SchalterAn");
+
+// console.log(class_List);
+
+// document.querySelector(".jumbotron").setAttribute("lang", "de");
+// document.querySelector("head > meta:nth-of-type(3)").removeAttribute("content");
+// console.log(document.querySelector("html").getAttribute("lang"));
+// console.log(document.querySelector(".jumbotron").hasAttribute("lang"));
+
+// let jumbo = document.querySelector(".jumbotron");
+// let jumboheader = document.querySelector(".jumbotron > header > h2");
+
+// console.log(jumbo.style);
+// jumbo.style.backgroundColor = "red";
+// jumbo.style.color = "white";
+// jumbo.style.border = "8px solid black";
+// jumbo.style.boxSizing = "borderbox";
+// jumbo.style.width = "600px";
+// jumbo.style.fontSize = "10px";
+
+// jumboheader.style.fontSize = "14px";
+
+// console.log(window);
+
+// console.log(window.document.title);
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+// console.log(window.outerWidth);
+// console.log(window.outerHeight);
+// console.log(window.scrollX);
+// console.log(window.scrollY);
+
+// console.log(window.location);
+// console.log(window.localStorage);
+// console.log(window.sessionStorage);
+
+// alert("Achtung!");
+// console.log(confirm("Bist du doof?"));
+
+// let jumboheader = document.querySelector(".jumbotron > header > h2");
+// let prm = prompt("Was geht?");
+// jumboheader.innerHTML = prm;
+
+// print();
+// open("https://www.google.de");
+// close();
+
+// let jumboheader = document.querySelector(".jumbotron > header > h2");
+// let jumboStyle = getComputedStyle(jumboheader);
+// console.log(getComputedStyle(jumboheader));
+
+// console.log(jumboStyle);
+// console.log(jumboStyle.color);
+// console.log(jumboStyle.fontFamily);
+// console.log(jumboStyle.color);
+
+// console.log(location);
+// console.log(location.href);
+// console.log(location.protocol);
+// console.log(location.port);
+// console.log(location.pathname);
+
+// let monatsliste = document.querySelectorAll(".jumbotron section");
+// console.log(monatsliste);
+// monatsliste.forEach(e => {
+//         e.remove();
+//     });
+// console.log(monatsliste);
+
