c07af5861fa6de1a0e3eed76dc806967ad005a6f
Alexander Gerdt <info@gerdt-webdesign.de>
2024-04-05 00:08:07 +0200

    Teil 15 Projekt - Methoden umsetzen

---
+++ src/js/eingabeformular.js
@@ -0,0 +1,163 @@
+"use strict"
+
+const eingabeformular = {
+
+    formulardatenHolen(e) {
+
+        return {
+            titel: e.target[0].value,
+            betrag: e.target[3].value,
+            einnahme: e.target[1].checked,
+            ausgabe: e.target[2].checked,
+            datum: e.target[4].valueAsDate
+        }
+    },
+
+    formularDatenVerarbeiten(formulardaten){
+
+        let typ;
+        if (formulardaten.einnahme === true){
+            typ = "einnahme";
+        } else if (formulardaten.ausgabe === true){
+            typ = "ausgabe"
+        }
+
+        return {
+            titel: formulardaten.titel.trim(),
+            typ: typ,
+            betrag: parseFloat(formulardaten.betrag) * 100,
+            datum: formulardaten.datum,
+        }
+    },
+
+    formularDatenValidieren(formulardaten){
+        let fehler = []; 
+        
+        if (formulardaten.titel === ""){
+            fehler.push("Der Titel wurde nicht angegeben!");
+        }
+
+        if( formulardaten.typ === undefined || formulardaten.typ.match(/^(?:einnahme|ausgabe)$/) === null){
+            fehler.push("Der Typ ist ungültig!");
+        }
+        
+        if (isNaN(formulardaten.betrag) === true){
+            fehler.push("Der Typ Betrag ist unbekannt!");
+        }
+
+        if (formulardaten.datum === null){
+            fehler.push("Das Datumsformat ist unbekannt!");
+        }
+
+        return fehler;
+
+    },
+    
+    datumAktualisieren(){
+        let datumInput = document.querySelector("#datum");
+        if (datumInput !== null){
+            datumInput.valueAsDate = new Date();
+        }
+    },
+
+    absendeEventHinzufuegen(eingabeformular){
+        eingabeformular.querySelector("#eingabeformular").addEventListener("submit", e => {
+            e.preventDefault();
+            let formulardaten = this.formularDatenVerarbeiten(this.formulardatenHolen(e));
+            let formularFehler = this.formularDatenValidieren(formulardaten);
+            if(formularFehler.length === 0){
+                haushaltsbuch.eintrag_hinzufuegen(formulardaten);
+                this.fehlerBoxEntfernen();
+                e.target.reset(); 
+                this.datumAktualisieren();
+            }else { 
+                this.fehlerBoxEntfernen();
+                this.fehlerBoxAnzeigen(formularFehler);
+            } 
+        });
+    },
+
+    htmlFehlerboxGenerieren(formularFehler){
+
+        let fehlerbox = document.createElement("div");
+        fehlerbox.setAttribute("class", "fehlerbox");
+        
+        let fehlertext = document.createElement("span");
+        fehlertext.textContent = "Folgende Felder wurden nicht korrekt ausgefüllt:";
+        fehlerbox.insertAdjacentElement("afterbegin", fehlertext);
+
+        let fehlerliste = document.createElement("ul");
+        formularFehler.forEach(e => {
+            let fehlerlistenpunkt = document.createElement("li")
+            fehlerlistenpunkt.textContent = e;
+            fehlerliste.insertAdjacentElement("beforeend", fehlerlistenpunkt);
+        });
+
+        fehlerbox.insertAdjacentElement("beforeend", fehlerliste);
+
+        return fehlerbox;
+    },
+
+    fehlerBoxAnzeigen(formularFehler){
+
+        let eingabeformular = document.querySelector("#eingabeformular");
+        if (eingabeformular !== null) {
+            eingabeformular.insertAdjacentElement("afterbegin", this.htmlFehlerboxGenerieren(formularFehler));
+        }
+    },
+
+    fehlerBoxEntfernen(){
+        let bestehendeFehlerbox = document.querySelector(".fehlerbox");
+        if (bestehendeFehlerbox !== null){
+            bestehendeFehlerbox.remove();
+        }
+    },
+
+
+    html_generieren(){
+        let formularhtml = `<form id="eingabeformular" action="#" method="get"></form>
+        <div class="eingabeformular-zeile">
+            <h1>Neue Einnahme / Ausgabe hinzufügen</h1>
+        </div>
+        <div class="eingabeformular-zeile">
+            <div class="titel-typ-eingabe-gruppe">
+                <label for="titel">Titel</label>
+                <input type="text" id="titel" form="eingabeformular" name="titel" placeholder="z.B. Einkaufen" size="10" title="Titel des Eintrags" >
+                <input type="radio" id="einnahme" name="typ" value="einnahme" form="eingabeformular" title="Typ des Eintrags">
+                <label for="einnahme" title="Typ des Eintrags">Einnahme</label>
+                <input type="radio" id="ausgabe" name="typ" value="ausgabe" form="eingabeformular" title="Typ des Eintrags" checked>
+                <label for="ausgabe" title="Typ des Eintrags">Ausgabe</label>
+            </div>
+        </div>
+        <div class="eingabeformular-zeile">
+            <div class="betrag-datum-eingabe-gruppe">
+                <label for="betrag">Betrag</label>
+                <input type="number" id="betrag" name="betrag" form="eingabeformular" placeholder="z.B. 10,42" size="10" step="0.01" title="Betrag des Eintrags (max. zwei Nachkommastellen, kein €-Zeichen)" >
+                <label for="datum">Datum</label>
+                <input type="date" id="datum" name="datum" form="eingabeformular" placeholder="jjjj-mm-tt" size="10" title="Datum des Eintrags (Format: jjjj-mm-tt)" >
+            </div>
+        </div>
+        <div class="eingabeformular-zeile">
+            <button class="standard" type="submit" form="eingabeformular">Hinzufügen</button>
+        </div>`;
+        let eingabeformular = document.createElement("section");
+        eingabeformular.setAttribute("id", "eingabeformular-container");
+        eingabeformular.innerHTML = formularhtml;
+        
+        this.absendeEventHinzufuegen(eingabeformular);
+
+        return eingabeformular;
+    },
+
+    anzeigen(){
+        let nav = document.querySelector("#navigationsleiste");
+        nav.insertAdjacentElement("afterend", this.html_generieren());
+        this.datumAktualisieren();
+    }
+
+};
+
+eingabeformular.anzeigen();
+
+
+
--- ueben.js
+++ ueben.js
@@ -1,68 +1,145 @@
 "use strict"
 
-// const multiplizieren = function(a,b) { return a * b; };
-// const multiplizieren = (a,b) => { return a * b };
-// const multiplizieren = (a,b) => a * b ;
+// let jumbo = document.querySelector(".jumbotron");
 
-// const begruessung = function(name) { return "Hallo" + name; };
-// const begruessung = (name) => "Hallo " + name;
-// const begruessung = name => "Hallo " + name;
+// jumbo.addEventListener("click", e => {
+//     console.log(e);
+//     console.log(e.clientX);
+//     console.log(e.clientY);
+//     console.log(e.target);
+//     console.log(e.screenX);
+// });
 
-// const sinnDesLebens = function(){
-//     return 45;
-// };
-// const sinnDesLebens = () => 45;
+// jumbo.addEventListener("dblclick", e => {
+//     console.log(e);
+//     alert("Doppelklick");
+// });
 
-// let einkaufsliste = [
-//     "Bort",
-//     "Käse",
-//     "Milch",
-//     "Butter"
-// ];
+// jumbo.addEventListener("mousedown", e => {
+//     console.log(e);
+//     alert("Maus runter");
+// });
 
-// einkaufsliste.forEach(function(e){
+// jumbo.addEventListener("mouseup", e => {
+//     console.log(e);
+//     alert("Maus hoch");
+// });
+
+// jumbo.addEventListener("mouseover", e => {
+//     console.log(e);
+//     alert("Maus over");
+// });
+
+// jumbo.addEventListener("mouseout", e => {
+//     console.log(e);
+//     alert("Maus out");
+// });
+
+// let body = document.querySelector("body");
+// body.addEventListener("click", e => console.log("davor der Event"));
+
+
+// let el1 = document.querySelector("#navigation  ul > li:nth-of-type(1) a");
+// let el1Parent = el1.parentElement;
+// let el2 = document.querySelector("#navigation  ul > li:nth-of-type(2) a");
+// let el2Parent = el2.parentElement;
+
+// el1.addEventListener("click", e => {
+//     e.preventDefault();
+//     console.log("el1 mitbekommen");
+// });
+
+// el1Parent.addEventListener("click", e => {
+//     console.log("el1Parent mitbekommen");
+// });
+
+// el2.addEventListener("click", e => {
+//     e.preventDefault();
+//     e.stopImmediatePropagation();
+//     console.log("el2 mitbekommen");
+// });
+
+// el2Parent.addEventListener("click", e => {
+//     console.log("el2Parent mitbekommen");
+// });
+
+// document.addEventListener("keydown", e => {
+//     document.querySelector("h2").innerHTML = e.key;
+//     document.querySelector("h2").innerHTML = e.code;
+//     console.log(e);
+
+// });
+
+// document.addEventListener("keyup", e => {
+//     document.querySelector("h2").innerHTML = e.key;
+//     document.querySelector("h2").innerHTML = e.code;
+//     console.log(e);
+
+// });
+
+// document.addEventListener("keypress", e => {
+//     document.querySelector("h2").innerHTML = e.key;
+//     document.querySelector("h2").innerHTML = e.code;
+//     console.log(e);
+
+// });
+
+// let formular = document.querySelector("form");
+
+// formular.addEventListener("submit", e => {
+//     e.preventDefault();
 //     console.log(e);
 // });
 
-// einkaufsliste.forEach(e => console.log(e));
+// formular.addEventListener("reset", e => console.log(e));
 
-let peron1 = {
-    vorname: "Anna",
-    nachname: "Musterfrau",
-    alter: 24,
-    meineMethode() { console.log(this);}
-}
+// let input = document.querySelector("input[type=text]");
+// input.addEventListener("input", e => console.log(e));
+// input.addEventListener("change", e => { console.log(e)});
 
-peron1.meineMethode();
+// let textarea = document.querySelector("textarea");
+// textarea.addEventListener("input", e => console.log(e));
+// textarea.addEventListener("change", e => { console.log(e)});
 
-// Die Nutzung von this innerhalb einer Funktion "fällt das this aus dem Kontext"
-// Bei Stict-Mode ist es undefined
+// let checkboxen = document.querySelectorAll("input[type=checkbox]");
+// checkboxen.forEach(el => {
+//     el.addEventListener("change", e => {
+//         console.log(e);
+//         console.log(e.target.checked);
+//     })
+// });
 
-let person2 = {
-    vorname: "Anna",
-    nachname: "Musterfrau",
-    alter: 24,
-    meineMethode(){
-        const meineFunktion = function(){
-            console.log(this);
-        };
-        meineFunktion();
-    }
-}
+// let radiobuttons = document.querySelectorAll("input[type=radio]");
+// radiobuttons.forEach(el => {
+//     el.addEventListener("change", e => {
+//         console.log(e);
+//         console.log(e.target.value);
+//     })
+// });
 
-// Pfeil-Funktionen erhalten den Kontext zum Objekt in dem Sie erstellt werden.
+// let range = document.querySelector("input[type=range");
+// range.addEventListener("change", e => {
+//     console.log(e);
+//     console.log(e.target.value);
+// });
 
-person2.meineMethode();
+// window.addEventListener("resize", e => console.log(e.target.innerHeight));
+// window.addEventListener("scroll", e => {
+//     console.log(e);
+//     console.log(scrollX);
+//     console.log(scrollY);
+// });
 
-let person3 = {
-    vorname: "Anna",
-    nachname: "Musterfrau",
-    alter: 24,
-    meineMethode(){
-        const meineFunktion = () => console.log(this);
-        meineFunktion();
-    }
-}
+// window.addEventListener("load", e => console.log(e));
 
-person3.meineMethode();
+// let link = document.querySelector("a.active");
+// let input = document.querySelector("input[type=text]");
+// let formular = document.querySelector("form");
+
+// setTimeout(() => link.click(), 2000); 
+// setTimeout(() => input.focus(), 3000); 
+// setTimeout(() => input.blur(), 6000); 
+// setTimeout(() => formular.reset(), 9000);
+// setTimeout(() => formular.submit(), 12000); 
+
 
